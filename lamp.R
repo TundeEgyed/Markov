07f517f3702a747cd786cee2acf189103c25c484
Tunde <egyed.tunde95@gmail.com>
2023-05-07 13:56:15 +0200

    lamp javítás

--- lamp.R
+++ lamp.R
@@ -1,40 +1,30 @@
 #LAMP
-shift <- function(mu, sample) {
-  a = t(head(sample, -mu))
-  return(rbind(a, sample[0:-mu]))
-}
-
-calculateTransitions <- function(mu, sample) {
-  transitions = shift(mu, sample)
-  transitions = data.frame(t(matrix(transitions, nrow = 2)))
-  
-  return(transitions)
-}
-
-calculateFrequencies <- function(mu, sample) {
-  frequencies = calculateTransitions(mu, sample)
-  names(frequencies) <- c("From", "To")
-  
-  return(table(frequencies))
-}
-
-order = 1
-possibleValues = unique(sample)
+order = 2
+possibleValues = sort(unique(sample))
 n = length(possibleValues)
 psi = rep(1 / order, order)
-a = array(dim = c(n, n, order))
+a = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
 for (i in 1:order) {
   a[,,i] = matrix(rep(1 / n, n * n), nrow = n)
 }
+psi2 = vector(length = order)
+a2 = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
 
-for (i in 1:100) {
-  for (j in 1:order) {
-    psi[j] = psi[j] * sum(calculateFrequencies(j, sample) * a[,,j])
+for (j in 1:100) {
+  a2[1 == 1] = 0
+  for (mu in 1:order) {
+    psi2[mu] = 0
+    for (i in (order + 1):length(sample)) {
+      denominator = 0
+      for (nu in 1:order) {
+        denominator = denominator + (psi[nu] * a[sample[i - nu], sample[i], nu])
+      }
+      psi2[mu] = psi2[mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
+      a2[sample[i - mu], sample[i], mu] = a2[sample[i - mu], sample[i], mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
+    }
   }
-  psi = psi / sum(psi)
-  
-  for (j in 1:order) {
-    a[,,j] = psi[j] * calculateFrequencies(j, sample) * a[,,j]
-    a[,,j] = a[,,j] / rowSums(a[,,j])
+  psi = psi2 / sum(psi2)
+  for (mu in 1:order) {
+    a[,,mu] = a2[,,mu] / rowSums(a2[,,mu])
   }
 }
