fdcfb75041014f6fa1fc28eb4f654bf422deaecf
Tunde <egyed.tunde95@gmail.com>
2023-05-14 22:02:09 +0200

    Akakike

--- lamp.R
+++ lamp.R
@@ -25,53 +25,65 @@ calculateRealtiveFrequencies <- function(mu, sample) {
   return(realtiveFrequencies)
 }
 
-order = 2
 numberOfIterations = 100
 possibleValues = sort(unique(sample))
 n = length(possibleValues)
-psi = runif(order, 0, 1)
-psi = psi / sum(psi) #véletlen indítás
-#psi = rep(1 / order, order) # egyenletes
-a = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
-for (i in 1:order) {
-  a[,,i] = calculateRealtiveFrequencies(mu, sample)
-}
-for (i in 1:order) {
-  ###relativ gyak
-  #a[,,i] = calculateRealtiveFrequencies(mu, sample)
-  ###véletlen
-  #a[,,i] = matrix(runif(n * n, 0, 1), nrow = n)
-  #a[,,i] = a[,,i] / rowSums(a[,,i])
-  ###egyenletes
-  a[,,i] = matrix(rep(1 / n, n * n), nrow = n)
-}
-psi2 = vector(length = order)
-a2 = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
-loglikelihood = rep(0, numberOfIterations)
 
-for (j in 1:numberOfIterations) {
-  a2[1 == 1] = 0
-  for (mu in 1:order) {
-    psi2[mu] = 0
-    for (i in (order + 1):length(sample)) {
-      denominator = 0
-      for (nu in 1:order) {
-        denominator = denominator + (psi[nu] * a[sample[i - nu], sample[i], nu])
-      }
-      psi2[mu] = psi2[mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
-      a2[sample[i - mu], sample[i], mu] = a2[sample[i - mu], sample[i], mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
-    }
+calculateLamp <- function(order) {
+  psi = runif(order, 0, 1)
+  #psi = psi / sum(psi) #véletlen indítás
+  psi = rep(1 / order, order) # egyenletes
+  a = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
+  for (i in 1:order) {
+    a[,,i] = calculateRealtiveFrequencies(mu, sample)
   }
-  for (i in (order + 1):length(sample)) {
-    p = 0
+  for (i in 1:order) {
+    ###relativ gyak
+    #a[,,i] = calculateRealtiveFrequencies(mu, sample)
+    ###véletlen
+    #a[,,i] = matrix(runif(n * n, 0, 1), nrow = n)
+    #a[,,i] = a[,,i] / rowSums(a[,,i])
+    ###egyenletes
+    a[,,i] = matrix(rep(1 / n, n * n), nrow = n)
+  }
+  psi2 = vector(length = order)
+  a2 = array(dim = c(n, n, order), dimname = list(possibleValues, possibleValues))
+  loglikelihood = rep(0, numberOfIterations)
+  
+  for (j in 1:numberOfIterations) {
+    a2[1 == 1] = 0
     for (mu in 1:order) {
-      p = p + psi[mu] * a[sample[i - mu], sample[i], mu] 
+      psi2[mu] = 0
+      for (i in (order + 1):length(sample)) {
+        denominator = 0
+        for (nu in 1:order) {
+          denominator = denominator + (psi[nu] * a[sample[i - nu], sample[i], nu])
+        }
+        psi2[mu] = psi2[mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
+        a2[sample[i - mu], sample[i], mu] = a2[sample[i - mu], sample[i], mu] + (psi[mu] * a[sample[i - mu], sample[i], mu] / denominator)
+      }
     }
-    loglikelihood[j] = loglikelihood[j] + log(p)
+    for (i in (order + 1):length(sample)) {
+      p = 0
+      for (mu in 1:order) {
+        p = p + psi[mu] * a[sample[i - mu], sample[i], mu] 
+      }
+      loglikelihood[j] = loglikelihood[j] + log(p)
+    }
+    psi = psi2 / sum(psi2)
+    for (mu in 1:order) {
+      a[,,mu] = a2[,,mu] / rowSums(a2[,,mu])
+    }
   }
-  psi = psi2 / sum(psi2)
-  for (mu in 1:order) {
-    a[,,mu] = a2[,,mu] / rowSums(a2[,,mu])
-  }
+  #a = round(a, 3)
+  
+  return(loglikelihood[numberOfIterations])
 }
-a = round(a, 3)
+
+calculateAkaikeLamp <- function(order) {
+  AIC = 2 * order * n * (n - 1) + n - 1 - 2 * calculateLamp(order)
+  
+  return(AIC)
+}
+calculateAkaikeLamp(1) - calculateAkaikeLamp(3)
+calculateAkaikeLamp(2) - calculateAkaikeLamp(3)
